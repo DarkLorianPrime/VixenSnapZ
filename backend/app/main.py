c7aafc835c03add76a282f323594f253718fbb9e
DarkLorianPrime <lolpokens@mail.ru>
2023-05-21 01:54:56 +0400

    fix bug and add minio volume

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.encoders import jsonable_encoder
+from pydantic import ValidationError
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 from libraries.database import engine, base
 from routers.authorization import authorization
@@ -11,6 +16,15 @@ async def create_database():
 
 
 app = FastAPI(title="DarkFileServer", version="2.3.5", on_startup=[create_database])
+
+
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(_: Request, exc: ValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors(), "Error": "Name field is missing"}),
+    )
+
 router = APIRouter(prefix="/api/v1")
 router.include_router(authorization.router)
 router.include_router(frames.router)
